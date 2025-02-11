79bb9f8d89f6f7559191dca43f4456a4bc109d28
Zahari.G.Bechtle-Notebook <george.zahari@gmail.com>
2025-02-11 10:31:03 +0100

    first upload of my mini citrix api call script. added: - gitignore - api.ps1 - README.md

---
+++ README.md
@@ -0,0 +1,114 @@
+
+<div align="center">
+    <a href="ZahariG" target="_blank">
+        <img src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2020/09/b960fb_ed442c56da8d41d9a27a1bade24fe275mv2.jpg?fit=1568%2C882&quality=89&ssl=1" 
+        alt="Logo" height="300" width="auto">
+    </a>
+</div>
+
+## About
+
+* this is a simple Citrix-API script in PS
+* this script querys data from the Cloud
+* you need a "secureclient.csv" located in root containing your Citrix credentials in order to authenticate and query data
+* you can also optionally hardcode your credentials, but its not recommended
+
+
+## How to use this project
+
+* the usage should be selfexplanatory, else please dont use it
+* as mentioned above, you need Citrix credentials to authenticate (either in a csv or hardcoded)
+* this small script is used for testing purposes and later to expand into smthing more serious
+
+## Demo<!-- Required -->
+
+* In construction!
+
+
+## Table of Contents<!-- Optional -->
+
+* In construction!
+
+<dev align="center">
+<table align="center">
+        <tr>
+            <td><a href="#about">About</a></td>        
+            <td><a href="#how-to-use-this-project">Getting started</td>
+            <td><a href="#demo">Demo</a></td>
+            <td><a href="#project-roadmap--">Project Roadmap</a></td>
+            <td><a href="#documentation">Documentation</a></td>
+        </tr>
+        <tr>
+            <td><a href="#contributors">Contributors</a></td>
+            <td><a href="#acknowledgments">Acknowledgments</a></td>
+            <td><a href="#feedback">Feedback</a></td>
+            <td><a href="#contact">Contact</a></td>
+            <td><a href="#license">License</a></td>
+        </tr>
+</table>
+</dev>
+
+
+<!-- - Use this html element to create a back to top button. -->
+<p align="right"><a href="#how-to-use-this-project">back to top ⬆️</a></p>
+
+
+## Project Roadmap <!-- Optional --> <!-- add learning_Rs-->
+<!-- 
+* Add this section in case the project has different phases
+* 
+* Under production or will be updated.
+-->
+
+* In construction!
+
+<p align="right"><a href="#how-to-use-this-project">back to top ⬆️</a></p>
+
+
+
+## Documentation<!-- Optional -->
+<!-- 
+* You may add any documentation or Wikis here
+* 
+* 
+-->
+
+
+## Contributors<!-- Required -->
+
+* Just me,myself and I atm
+<!-- 
+* Without contribution we wouldn't have open source. 
+* 
+* Generate github contributors Image here https://contrib.rocks/preview?repo=angular%2Fangular-ja
+-->
+
+
+<!-- Optional -->
+<!-- 
+## Acknowledgments
+* Credit where it's do 
+* 
+* Feel free to share your inspiration sources, Stackoverflow questions, github repos, tools etc.
+-->
+
+
+<!-- - Use this html element to create a back to top button. -->
+<p align="right"><a href="#how-to-use-this-project">back to top ⬆️</a></p>
+
+
+## Feedback<!-- Required -->
+
+zahari.g.office@gmail.com
+
+## Contact<!-- Required -->
+
+zahari.g.office@gmail.com
+
+
+## License<!-- Optional -->
+MIT
+
+
+<!-- - Use this html element to create a back to top button. -->
+<p align="right"><a href="#how-to-use-this-project">back to top ⬆️</a></p>
---
+++ api.ps1
@@ -0,0 +1,124 @@
+﻿# TODAYS DATE AS STARTPARAMETER IF NONE IS HANDED WITH START OF SCRIPT
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$startDate = (Get-Date -Hour 0 -Minute 0 -Second 0).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+
+    [Parameter(Mandatory=$false)]
+    [string]$endDate = (Get-Date -Hour 23 -Minute 59 -Second 59).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+)
+
+# IMPORT CREDENTIALS FROM secureclient.csv
+$path ="./secureclient.csv"
+$credentials = Import-Csv -Path $path
+
+foreach ($row in $credentials) {
+    #$name = $row."Name"  ---> Not needed atm
+    $clientID = $row."Client-ID"
+    $clientSecret = $row."Secret"
+    $customerID = $row."Customer-ID"
+}
+
+# URLS 
+$bearerTokenUrl = "https://api-eu.cloud.com/cctrustoauth2/$customerID/tokens/clients"   # Get-BearerToken
+$siteIdUrl = "https://api.cloud.com/cvad/manage/me";                                    # Get-SiteID
+$apiEndpointUrl = "https://api-eu.cloud.com/monitorodata/";                             # Execute-Odata
+
+# BEARER TOKEN #############################################################
+function Get-BearerToken {
+    
+    $headers = @{
+            "Accept" = "application/json"
+            "Content-Type" = "application/x-www-form-urlencoded"
+        }
+
+    $body = @{        
+        "grant_type" = "client_credentials"
+        "client_id" = $clientID
+        "client_secret" = $clientSecret
+    }
+
+    try {
+        $response = Invoke-RestMethod `
+        -Headers $headers `
+        -Method POST `
+        -Uri $bearerTokenUrl `
+        -Body $body `
+        -ContentType "application/x-www-form-urlencoded"
+
+        Write-Host "Amazing! Bearer-Token was aquired and expires in $($response.expires_in) seconds!" -ForegroundColor White -BackgroundColor DarkGreen
+        return $response.access_token
+
+    }catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red 
+        Write-Host $_ -ForegroundColor Yellow
+        Write-Host "Could not get bearer token. Abort..." -ForegroundColor Red
+        break
+    }
+}
+$bearerToken = Get-BearerToken 
+
+# SITE ID ##################################################################
+function Get-SiteID{
+    $headers = @{
+        "Accept" = "application/json"
+        "Authorization" = "CWSAuth Bearer=$bearerToken"
+        "Citrix-CustomerId" = $customerID
+    }
+    
+    try{
+    
+    $response = Invoke-RestMethod `
+        -Uri $siteIdUrl `
+        -Method GET `
+        -Headers $headers
+        
+        Write-Host "Amazing! SiteID was aquired." -ForegroundColor White -BackgroundColor DarkGreen
+        return $response.Customers.Sites.Id
+
+    }catch{
+        Write-Host $_.Exception.Message -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host $_ -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host "Could not get SITE-ID ..." -ForegroundColor White -BackgroundColor Red
+    }
+    
+}
+$siteID = Get-SiteID
+
+# API QUERRY ###############################################################
+function Execute-Odata($resource){
+    
+    $headers = @{        
+        "Accept" = "application/json"
+        "charset" = "utf-8"
+        "Authorization" = "CWSAuth Bearer=$bearerToken"
+        "Citrix-CustomerId" = $customerID
+        "Citrix-InstanceId" = $siteID
+        "Content-Type" = "application/json"
+    }
+
+    $uri = "${apiEndpointUrl}${resource}"
+    Write-Host $uri
+
+    try {
+        $response = Invoke-RestMethod `
+            -Headers $headers `
+            -Method GET `
+            -Uri $uri `
+            -ContentType "application/x-www-form-urlencoded"
+            
+        return $response.value
+        
+    } catch {
+        Write-Host $_.Exception.Message -ForegroundColor Yellow
+        Write-Host "Could not get DATA, Abort ..." -BackgroundColor Red
+        
+    }
+}
+# Write-Host "###### Get all NON active machines ##########"
+# Execute-Odata -resource 'Machines?$filter=LifecycleState eq 1 ' | Format-Table
+
+Write-Host "###### Get all machines with SELECTED FILEDS ##########"
+Execute-Odata -resource 'Machines?$select=Name,LifecycleState,IsInMaintenanceMode,IsAssigned,CurrentPowerState,ModifiedDate ' | Format-Table
+
+
+
