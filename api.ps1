bc182b3cedb350dc74c3b52d686bf16aa52dfdb3
Zahari.G.Bechtle-Notebook <george.zahari@gmail.com>
2025-02-14 23:45:12 +0100

    - Completely reworked the script so you can limit the fields you will get as an answer. - The request gets now saved as a .JSON in ./logs/{$timestamp}/ - moved all funtions to a seperate module in a separate module folder, so the main part remains clean

--- api.ps1
+++ api.ps1
@@ -1,16 +1,9 @@
-﻿# TODAYS DATE AS STARTPARAMETER IF NONE IS HANDED WITH START OF SCRIPT
-param(
-    [Parameter(Mandatory=$false)]
-    [string]$startDate = (Get-Date -Hour 0 -Minute 0 -Second 0).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+﻿# Import-Module "./modules/common.config.psm1";
+Import-Module "./modules/common.functions.psm1";
 
-    [Parameter(Mandatory=$false)]
-    [string]$endDate = (Get-Date -Hour 23 -Minute 59 -Second 59).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-)
-
-# IMPORT CREDENTIALS FROM secureclient.csv
+# IMPORTING CREDENTIALS FROM CSV-FILE
 $path ="./secureclient.csv"
 $credentials = Import-Csv -Path $path
-
 foreach ($row in $credentials) {
     #$name = $row."Name"  ---> Not needed atm
     $clientID = $row."Client-ID"
@@ -18,107 +11,32 @@ foreach ($row in $credentials) {
     $customerID = $row."Customer-ID"
 }
 
-# URLS 
+# URLS #################################################################### 
 $bearerTokenUrl = "https://api-eu.cloud.com/cctrustoauth2/$customerID/tokens/clients"   # Get-BearerToken
-$siteIdUrl = "https://api.cloud.com/cvad/manage/me";                                    # Get-SiteID
-$apiEndpointUrl = "https://api-eu.cloud.com/monitorodata/";                             # Execute-Odata
+$siteIdUrl = "https://api.cloud.com/cvad/manage/me"                                     # Get-SiteID
+$apiEndpointUrl = "https://api.cloud.com/cvad/manage/"                                  # Execute-Odata
+
+$root = "./logs" # WHERE DO TOU WANT TO SAVE THE JSON RESULTS
 
 # BEARER TOKEN #############################################################
-function Get-BearerToken {
-    
-    $headers = @{
-            "Accept" = "application/json"
-            "Content-Type" = "application/x-www-form-urlencoded"
-        }
-
-    $body = @{        
-        "grant_type" = "client_credentials"
-        "client_id" = $clientID
-        "client_secret" = $clientSecret
-    }
-
-    try {
-        $response = Invoke-RestMethod `
-        -Headers $headers `
-        -Method POST `
-        -Uri $bearerTokenUrl `
-        -Body $body `
-        -ContentType "application/x-www-form-urlencoded"
-
-        Write-Host "Amazing! Bearer-Token was aquired and expires in $($response.expires_in) seconds!" -ForegroundColor White -BackgroundColor DarkGreen
-        return $response.access_token
-
-    }catch {
-        Write-Host $_.Exception.Message -ForegroundColor Red 
-        Write-Host $_ -ForegroundColor Yellow
-        Write-Host "Could not get bearer token. Abort..." -ForegroundColor Red
-        break
-    }
-}
-$bearerToken = Get-BearerToken 
-
+$bearerToken = Get-BearerToken $clientID $clientSecret $bearerTokenUrl
+# Write-Host $bearerToken
 # SITE ID ##################################################################
-function Get-SiteID{
-    $headers = @{
-        "Accept" = "application/json"
-        "Authorization" = "CWSAuth Bearer=$bearerToken"
-        "Citrix-CustomerId" = $customerID
-    }
-    
-    try{
-    
-    $response = Invoke-RestMethod `
-        -Uri $siteIdUrl `
-        -Method GET `
-        -Headers $headers
-        
-        Write-Host "Amazing! SiteID was aquired." -ForegroundColor White -BackgroundColor DarkGreen
-        return $response.Customers.Sites.Id
+$siteID = Get-SiteID $customerID $bearerToken $siteIdUrl
+# Write-Host $siteID 
+# Create New Folder for JSON to be saved to ################################
+$folderName = ""
+$folderName = New-Folder $root
+# Create Filter to get only the Fields we need #############################
+$filter = "?fields=Name,PowerState,RegistrationState,FaultState,LastErrorTime,LastErrorReason"
 
-    }catch{
-        Write-Host $_.Exception.Message -ForegroundColor Red -BackgroundColor Yellow
-        Write-Host $_ -ForegroundColor Red -BackgroundColor Yellow
-        Write-Host "Could not get SITE-ID ..." -ForegroundColor White -BackgroundColor Red
-    }
-    
-}
-$siteID = Get-SiteID
+Write-Host "###### YES MASTER, I WILL LOOK AND FIND WHAT YOU NEED ! ...  ##########"
+$data = Get-Data $bearerToken $customerID $siteID $apiEndpointUrl $folderName $root 'Machines' $filter 
 
-# API QUERRY ###############################################################
-function Execute-Odata($resource){
-    
-    $headers = @{        
-        "Accept" = "application/json"
-        "charset" = "utf-8"
-        "Authorization" = "CWSAuth Bearer=$bearerToken"
-        "Citrix-CustomerId" = $customerID
-        "Citrix-InstanceId" = $siteID
-        "Content-Type" = "application/json"
-    }
 
-    $uri = "${apiEndpointUrl}${resource}"
-    Write-Host $uri
 
-    try {
-        $response = Invoke-RestMethod `
-            -Headers $headers `
-            -Method GET `
-            -Uri $uri `
-            -ContentType "application/x-www-form-urlencoded"
-            
-        return $response.value
-        
-    } catch {
-        Write-Host $_.Exception.Message -ForegroundColor Yellow
-        Write-Host "Could not get DATA, Abort ..." -BackgroundColor Red
-        
-    }
-}
-# Write-Host "###### Get all NON active machines ##########"
-# Execute-Odata -resource 'Machines?$filter=LifecycleState eq 1 ' | Format-Table
 
-Write-Host "###### Get all machines with SELECTED FILEDS ##########"
-Execute-Odata -resource 'Machines?$select=Name,LifecycleState,IsInMaintenanceMode,IsAssigned,CurrentPowerState,ModifiedDate ' | Format-Table
+
 
 
 
---
+++ modules/common.functions.psm1
@@ -0,0 +1,163 @@
+## AQUIRING BEARER TOKEN 
+function Get-BearerToken{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$clientID,
+        [Parameter(Mandatory=$true)]
+        [string]$clientSecret,
+        [Parameter(Mandatory=$true)]
+        [string]$bearerTokenUrl
+    )
+    $headers = @{
+            "Accept" = "application/json"
+            "Content-Type" = "application/x-www-form-urlencoded"
+        }
+
+    $body = @{        
+        "grant_type" = "client_credentials"
+        "client_id" = $clientID
+        "client_secret" = $clientSecret
+    }
+
+    try {
+        $response = Invoke-RestMethod `
+        -Headers $headers `
+        -Method POST `
+        -Uri $bearerTokenUrl `
+        -Body $body `
+        -ContentType "application/x-www-form-urlencoded"
+
+        Write-Host "Amazing! Bearer-Token was aquired and expires in $($response.expires_in) seconds!" -ForegroundColor White -BackgroundColor DarkGreen
+        return $response.access_token
+
+    }catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red 
+        Write-Host $_ -ForegroundColor Yellow
+        Write-Host "Could not get bearer token. Abort..." -ForegroundColor Red
+        break
+    }
+}
+
+## AQUIRING SITE-ID
+function Get-SiteID{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$customerID,
+        [Parameter(Mandatory=$true)]
+        [string]$bearerToken,
+        [Parameter(Mandatory=$true)]
+        [string]$siteIdUrl
+    )
+
+    $headers = @{
+        "Accept" = "application/json"
+        "Authorization" = "CWSAuth Bearer=$bearerToken"
+        "Citrix-CustomerId" = $customerID
+    }
+    
+    try{
+    
+    $response = Invoke-RestMethod `
+    -Uri $siteIdUrl `
+    -Method GET `
+    -Headers $headers
+    
+    Write-Host "Amazing! SiteID was aquired too." -ForegroundColor White -BackgroundColor DarkGreen
+    return $response.Customers.Sites.Id
+    
+    }catch{
+        Write-Host $_.Exception.Message -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host $_ -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host "Could not get SITE-ID ..." -ForegroundColor White -BackgroundColor Red
+        break
+    }
+
+}
+
+## API QUERRY 
+function Get-Data{
+    
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$bearerToken,
+        [Parameter(Mandatory=$true)]
+        [string]$customerID,
+        [Parameter(Mandatory=$true)]
+        [string]$siteID,
+        [Parameter(Mandatory=$true)]
+        [string]$apiEndpointUrl,
+        [Parameter(Mandatory=$true)]
+        [string]$folderName,
+        [Parameter(Mandatory=$true)]
+        [string]$root,
+        [Parameter(Mandatory=$true)]
+        [string]$resource,
+        [Parameter(Mandatory=$true)]
+        [string]$filter
+    )
+
+    $timestampFile = (Get-Date).ToString("yyyy-MM-dd_HH-mm")
+    $requestUri = "${apiEndpointUrl}${resource}${filter}"
+    
+    # Write-Host $requestUri
+    
+    $headers = @{        
+        "Accept" = "application/json"
+        "Authorization" = "CWSAuth Bearer=$bearerToken"
+        "Citrix-CustomerId" = $customerID
+        "Citrix-InstanceId" = $siteID
+    }
+    
+    try {
+        $response = Invoke-RestMethod `
+        -Headers $headers `
+        -Method GET `
+        -Uri $requestUri `
+        # -ContentType "application/x-www-form-urlencoded"
+        
+        $jsonFileName = "${timestampFile}_${resource}.json"
+        $jsonFilePath = "$root/$folderName/$jsonFileName"
+        $response | ConvertTo-Json -Depth 10 | Set-Content -Path $jsonFilePath
+        
+        Write-Host "Success" -ForegroundColor DarkGreen
+        return $true
+        
+    } catch {
+        Write-Host $_.Exception.Message -ForegroundColor Yellow
+        Write-Host "Could not get DATA, Abort ..." -BackgroundColor Red
+        return $false
+    }
+}
+
+## CREATE FOLDER
+function New-Folder{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$root
+    )
+    # check if there is a folder with today's date, else create new folder with today's date
+    $timestampFolder = Get-Date -Format "yyyy-MM-dd_HH-mm"
+    $folderName = "${timestampFolder}"
+    if (!(Test-Path -Path "$root/$folderName" -PathType Container)) {
+        New-Item -Path "$root/$folderName/" -ItemType Directory | Out-Null
+    }
+    return $folderName;
+}
+
+
+
+
+
+
+
+# IF DATE WILL BE NEEDED OR RELEVANT:
+##################################################################################################
+
+# TODAYS DATE AS STARTPARAMETER IF NONE IS HANDED WITH START OF SCRIPT
+# param(
+#     [Parameter(Mandatory=$false)]
+#     [string]$startDate = (Get-Date -Hour 0 -Minute 0 -Second 0).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+    
+#     [Parameter(Mandatory=$false)]
+#     [string]$endDate = (Get-Date -Hour 23 -Minute 59 -Second 59).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+#     )
